06b2400b4fc588cf71f3e9def08396583cc3175d
Aditya Agarwal <adityaa803@gmail.com>
2021-09-04 16:59:46 +0530

    provide list of routes in sidebar

--- packages/core/src/routing/getSidebarItems.js
+++ packages/core/src/routing/getSidebarItems.js
@@ -1,16 +1,22 @@
 import { routeTree } from './routeTree';
 
+function getTitle(route) {
+  const title = route.meta?.routeConfig?.title;
+  const defaultTitle = `To ${route.route}`;
+
+  return title || defaultTitle;
+}
+
 function getNavlinks() {
-  return [
-    {
-      title: 'Billing',
-      to: '/billing',
-    },
-    {
-      title: 'Users Details',
-      to: '/users',
-    },
-  ];
+  const routes = Object.values(routeTree);
+  const navlinks = routes.map((route) => {
+    return {
+      title: getTitle(route),
+      to: route.route,
+    };
+  });
+
+  return navlinks;
 }
 
 export function getSidebarItems() {
